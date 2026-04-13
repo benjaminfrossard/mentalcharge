f5061519bf9a643348f4a7c8691d98eec15f6d27
benjaminfrossard <benjamin.frossard@gmail.com>
2026-04-13 13:04:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v15'; // ← bumper à chaque déploiement
+const CACHE_VERSION = 'v16'; // ← bumper à chaque déploiement
 const CACHE_NAME = `mentalcharge-${CACHE_VERSION}`;
 const ASSETS = [
   './index.html',
